9ddbe2aae1a765d2889c74b35efd01e366cfeec6
Cryptoball <cryptoball7@gmail.com>
2026-04-28 15:23:53 -0700

    Fixed incorrect enqueue styles error

--- simple-service-marketplace/simple-service-marketplace.php
+++ simple-service-marketplace/simple-service-marketplace.php
@@ -6,38 +6,44 @@
  * Author: Cryptoball cryptoball7@gmail.com
  */
 
-if (!defined('ABSPATH')) exit;
+if (!defined('ABSPATH'))
+    exit;
 
-class SSO_Plugin {
+class SSO_Plugin
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         add_action('init', [$this, 'register_post_types']);
         add_shortcode('sso_order_form', [$this, 'order_form_shortcode']);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
         add_action('wp_ajax_sso_submit_order', [$this, 'handle_order']);
         add_action('wp_ajax_nopriv_sso_submit_order', [$this, 'handle_order']);
 
-add_action('init', function() {
-    wp_register_script(
-        'sso-block',
-        plugin_dir_url(__FILE__) . 'block.js',
-        ['wp-blocks', 'wp-element', 'wp-editor'],
-        null,
-        true
-    );
+        add_action('init', function () {
+            wp_register_script(
+                'sso-block',
+                plugin_dir_url(__FILE__) . 'block.js',
+                ['wp-blocks', 'wp-element', 'wp-editor'],
+                null,
+                true
+            );
 
-    register_block_type('sso/order-form', [
-        'editor_script' => 'sso-block',
-        'render_callback' => function($attributes) {
-            return do_shortcode('[sso_order_form]');
-        }
-    ]);
-});
+            register_block_type('sso/order-form', [
+                'editor_script' => 'sso-block',
+                'render_callback' => function ($attributes) {
+                    return do_shortcode('[sso_order_form]');
+                }
+            ]);
+        });
 
-wp_enqueue_style('sso-css', plugin_dir_url(__FILE__) . 'sso.css');
+        add_action('wp_enqueue_scripts', function () {
+            wp_enqueue_style('sso-css', plugin_dir_url(__FILE__) . 'sso.css');
+        });
     }
 
-    public function register_post_types() {
+    public function register_post_types()
+    {
         register_post_type('sso_service', [
             'label' => 'Services',
             'public' => true,
@@ -52,43 +58,46 @@ wp_enqueue_style('sso-css', plugin_dir_url(__FILE__) . 'sso.css');
         ]);
     }
 
-    public function enqueue_scripts() {
+    public function enqueue_scripts()
+    {
         wp_enqueue_script('sso-js', plugin_dir_url(__FILE__) . 'sso.js', ['jquery'], null, true);
         wp_localize_script('sso-js', 'sso_ajax', [
             'url' => admin_url('admin-ajax.php')
         ]);
     }
 
-    public function order_form_shortcode($atts) {
+    public function order_form_shortcode($atts)
+    {
         ob_start(); ?>
         <form id="sso-order-form" class="sso-form" enctype="multipart/form-data">
-    <div class="sso-field">
-        <label>Name</label>
-        <input type="text" name="name" required>
-    </div>
+            <div class="sso-field">
+                <label>Name</label>
+                <input type="text" name="name" required>
+            </div>
 
-    <div class="sso-field">
-        <label>Email</label>
-        <input type="email" name="email" required>
-    </div>
+            <div class="sso-field">
+                <label>Email</label>
+                <input type="email" name="email" required>
+            </div>
 
-    <div class="sso-field">
-        <label>Project Details</label>
-        <textarea name="requirements"></textarea>
-    </div>
+            <div class="sso-field">
+                <label>Project Details</label>
+                <textarea name="requirements"></textarea>
+            </div>
 
-    <div class="sso-field">
-        <label>Upload Files</label>
-        <input type="file" name="file">
-    </div>
+            <div class="sso-field">
+                <label>Upload Files</label>
+                <input type="file" name="file">
+            </div>
 
-    <button class="sso-submit">Submit Order</button>
-</form>
+            <button class="sso-submit">Submit Order</button>
+        </form>
         <div id="sso-response"></div>
         <?php return ob_get_clean();
     }
 
-    public function handle_order() {
+    public function handle_order()
+    {
         $name = sanitize_text_field($_POST['name']);
         $email = sanitize_email($_POST['email']);
         $requirements = sanitize_textarea_field($_POST['requirements']);
@@ -120,7 +129,7 @@ wp_enqueue_style('sso-css', plugin_dir_url(__FILE__) . 'sso.css');
 new SSO_Plugin();
 
 // Messaging system (basic)
-add_action('init', function() {
+add_action('init', function () {
     register_post_type('sso_message', [
         'label' => 'Messages',
         'public' => false,
@@ -128,13 +137,14 @@ add_action('init', function() {
     ]);
 });
 
-add_shortcode('sso_order_view', function() {
+add_shortcode('sso_order_view', function () {
     $id = intval($_GET['id']);
     $key = sanitize_text_field($_GET['key']);
 
     $saved_key = get_post_meta($id, 'secret', true);
 
-    if ($key !== $saved_key) return 'Invalid access';
+    if ($key !== $saved_key)
+        return 'Invalid access';
 
     ob_start();
 
@@ -175,7 +185,7 @@ add_shortcode('sso_order_view', function() {
 });
 
 // WooCommerce integration toggle
-add_action('plugins_loaded', function() {
+add_action('plugins_loaded', function () {
     if (class_exists('WooCommerce')) {
         // Hook services into WooCommerce products
     }
